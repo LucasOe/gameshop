8c0011d30cdc14d6a32bee518d5b5bbd2584d746
LucasOe <lucas.oelker@gmx.de>
2019-10-05 22:06:51 +0200

    Add Databank intergration

--- shop/productlist.php
+++ shop/productlist.php
@@ -1,223 +1,28 @@
 <?php
-$product_name = array(
-	/* 00 */ "Vampire The Masquerade - Bloodlines 2",
-	/* 01 */ "The Sinking City",
-	/* 02 */ "Tom Clancy's The Division 2",
-	/* 03 */ "Spellbreak",
-	/* 04 */ "Phoenix Point",
-	/* 05 */ "Slime Rancher",
-	/* 06 */ "Metro Exodus",
-	/* 07 */ "Hades",
-	/* 08 */ "Ashen",
-	/* 09 */ "Far Cry Primal",
-	/* 10 */ "Darksiders III",
-	/* 11 */ "Tom Clancy's Ghost Reacon Wildlands",
-	/* 12 */ "Watch Dogs 2",
-	/* 13 */ "World War Z"
-);
+	$mysqli = new mysqli('localhost', 'root', 'root', 'shop');
+			
+    $products = $mysqli->query("SELECT * FROM products");
+    $editions = $mysqli->query("SELECT * FROM editions");
+    $prices = $mysqli->query("SELECT * FROM prices");
 
-$product_editions = array(
-	/* 00 */ array("Vampire The Masquerade - Bloodlines 2 - Standart Edition", "Vampire The Masquerade - Bloodlines 2 - Deluxe Edition", "Vampire The Masquerade - Bloodlines 2 - Premium Edition"),
-	/* 01 */ array("The Sinking City - Standart Edition", "The Sinking City - Necronomicon Edition"),
-	/* 02 */ array("Tom Clancy's The Division 2 - Standart Edition", "Tom Clancy's The Division 2 - Gold Edition", "Tom Clancy's The Division 2 - Ultimate Edition"),
-	/* 03 */ array("Spellbreak - Alpha", "Spellbreak - Mage-Paket", "Spellbreak - Battlemage-Paket", "Spellbreak - Master-Packet"),
-	/* 04 */ array("Phoenix Point"),
-	/* 05 */ array("Slime Rancher"),
-	/* 06 */ array("Metro Exodus - Standart Edition", "Metro Exodus - Gold Edition"),
-	/* 07 */ array("Hades"),
-	/* 08 */ array("Ashen"),
-	/* 09 */ array("Far Cry Primal"),
-	/* 10 */ array("Darksiders III", "Darksiders III - The Crucible"),
-	/* 11 */ array("Tom Clancy's Ghost Reacon Wildlands"),
-	/* 12 */ array("Watch Dogs 2"),
-	/* 13 */ array("World War Z"),
-);
+    $product_name = array();
+    $product_editions = array();
+    $product_price = array();
+    $product_developer = array();
+    $product_description = array();
+            
+    for($i = 0; $i < $products->num_rows; $i++) {
+        $products->data_seek($i);
+        $row_products = $products->fetch_assoc();
+        $editions->data_seek($i);
+        $row_editions = $editions->fetch_assoc();
+        $prices->data_seek($i);
+        $row_prices = $prices->fetch_assoc();
 
-$product_price = array(
-	/* 00 */ array(59.99, 69.99, 89.99),
-	/* 01 */ array(59.99, 74.99),
-	/* 02 */ array(59.99, 99.99, 119.99),
-	/* 03 */ array(00.00, 49.99, 79.99, 119.99),
-	/* 04 */ array(39.99),
-	/* 05 */ array(9.99),
-	/* 06 */ array(59.99, 84.99),
-	/* 07 */ array(17.49),
-	/* 08 */ array(39.99),
-	/* 09 */ array(49.99),
-	/* 10 */ array(59.99, 6.99),
-	/* 11 */ array(49.99),
-	/* 12 */ array(59.99),
-	/* 13 */ array(34.99)
-);
-
-$product_developer = array(
-	/* 00 */ "Paradox Interactive | Hardsuit Labs Inc.",
-	/* 01 */ "Big Ben Interactive | Frogwares",
-	/* 02 */ "Ubisoft | Massive Entertainment",
-	/* 03 */ "Proletariat, Inc.",
-	/* 04 */ "Snapshot Games",
-	/* 05 */ "Monomi Park",
-	/* 06 */ "Deep Silver | 4A Games",
-	/* 07 */ "Supergiant Games",
-	/* 08 */ "Focus Home Interactive",
-	/* 09 */ "Ubisoft Entertainment",
-	/* 10 */ "THQ Nordic | Gunfire Games",
-	/* 11 */ "Ubisoft Entertainment",
-	/* 12 */ "Ubisoft Entertainment",
-	/* 13 */ "Saber Interactive"
-);
-
-$product_description = array(
-	/* 00 */ "
-<t>Werde zum ultimativen Vampir.</t>
-<i>Deine durch vampirischen Terror gezeugte Existenz löst den Krieg um Seattles Bluthandel aus. Gehe ungemütliche Allianzen mit den Wesen ein, die die Stadt kontrollieren, und decke die sich ausbreitende Verschwörung auf, die Seattle in einen blutigen Bürgerkrieg mächtiger Vampirfraktionen gestürzt hat.</i>
-
-<b>Werde zum ultimativen Vampir.</b>
-Tauche ein in die Welt der Dunkelheit und lebe deine Vampir-Fantasien aus, in einer Stadt voller spannender Charaktere, die auf deine Entscheidungen reagieren. Du und deine einzigartigen Disziplinen seid eine Waffe in unserem antreibenden, schnellen Nahkampf-System. Deine Macht wächst, je weiter du voranschreitest, aber vergiss nicht, die Maskerade aufrecht zu erhalten und deine Menschlichkeit zu bewahren – denn ein Verstoß hat ernste Konsequenzen.
-
-<b>Steige hinab in Seattles Herz der Finsternis
-und überlebe die Elite der Vampire</b>
-Seattle wurde seit jeher von Vampiren regiert. Jage deine Beute durch Schauplätze in Seattle, die originalgetreu in die Welt der Finsternis übertragen wurden. Triff die Begründer des alten Blutes, die seit dem Anbeginn der Stadt dort weilen, und die Reichen des neuen Blutes, die die Stadt neu gestalten. Jeder hat geheime Pläne – als wähle deine Verbündeten mit Bedacht.
-
-<b>Gehe ungemütliche Bündnisse ein</b>
-Wähle im Kampf um Seattles Bluthandel eine Seite unter den wetteifernden Fraktionen, jede mit eigenen Stärken und Hintergrundgeschichten. Die Welt beurteilt dich nach der Gesellschaft, die du dir wählst – Aber denke daran: Niemand bleibt für immer unbefleckt.
-
-<b>Erlebe die Geschichte</b>
-Verfasst vom kreativen Geist hinter dem original Bloodlines, Vampire: The Masquerade® - Bloodlines™ 2 wird den Ambitionen des ersten Spiels gerecht und holt einige der beliebtesten Figuren der Fans zurück.
-
-Erstellt von Hardsuit Labs / Published by Paradox Interactive
-",
-	/* 01 */ "
-<t>Stürze dich in den Wahnsinn</t>
-The Sinking City ist ein Abenteuer- und Detektivspiel inspiriert an der Welt von H.P. Lovecraft, dem Meister des Grauens.
-
-Tritt in die Fußstapfen eines Detektivs in den 1920er Jahren, ein getriebener Wahrheitsuchender, der in ein Abenteuer hineingezogen wird, dass über seine Ziele und Überzeugungen hinausgeht.
-
-Gefangen in einer überschwemmten Stadt, die du frei erkunden kannst, schmuggelst du dich bei einem Geheimbund ein und untersuchst Fälle, die die geistige Gesundheit auf eine harte Probe stellen. Versuche zu retten, was du kannst – aber vor allem dich selbst.
-",
-	/* 02 */ "
-<t>Die Nachwelt wird sich erinnern</t>
-In Tom Clancy’s The Division 2 steht das Schicksal der freien Welt auf dem Spiel.
-
-Führe ein Team von Eliteagenten in das von der Seuche verheerte Washington DC, um die Ordnung wiederherzustellen und die Stadt zu retten.
-
-The Division 2 ist ein Online-Actionshooter-Rollenspielerlebnis, in dem Erkundung und Charakterentwicklung im Vordergrund stehen.
-
-Befreie Washington DC – Rette das vor dem Untergang stehende Land und erkunde eine offene, dynamische und feindselige Welt.
-
-Stärker mit deinem Team – Spezialisiere dich und kämpfe zusammen mit deinen Freunden in Online-Koop-Partien oder gegeneinander im herausfordernden PvP-Modus.
-
-Genre-prägendes Endspiel – Wähle eine einzigartige Klassenspezialisierung, um dich mit deinen Freunden den härtesten Herausforderungen zu stellen.
-",
-	/* 03 */ "
-<t>Bereit zum Aufbruch</t>
-Spellbreak ist ein neues Battle-Royale-Rollenspiel mit sagenhaften magischen Kämpfen. Verknüpfe Zaubersprüche zu spektakulären Kombinationen und baue deine Fähigkeiten strategisch klug auf.
-
-Spellbreak befindet sich in der geschlossenen Alpha! Das bedeutet, dass wir aktiv an der Entwicklung arbeiten. Wir liefern stetig neue Inhalte, beheben Fehler und verbessern die Balance. Damit das Spiel so gut wie möglich werden kann, brauchen wir deine Hilfe!
-
-Hinweis: Das Spiel ist aktuell nur auf Englisch verfügbar.
-
-Erstellt von Proletariat
-",
-	/* 04 */ "
-<t>Phoenix Point</t>
-Phoenix Point ist das neue Strategiespiel des Machers der original X-COM-Serie. Es verbindet rundenbasierte Taktik mit weltumspannender Strategie im Kampf gegen eine furchteinflößende außerirdische Bedrohung. Dein Gegner mutiert und entwickelt sich, um sich an deine Taktiken anzupassen und sorgt so für eine Bandbreite an Herausforderungen und Überraschungen auf dem Schlachtfeld.
-
-Erstellt von Snapshot Games
-",
-	/* 05 */ "
-<t>Willkommen auf der „Fernen, fernen Weite“</t>
-Slime Rancher entführt dich in eine charmante, offene Welt! Spiele als Beatrix LeBeau: eine mutige Rancherin, die viele Lichtjahre von der Erde entfernt ein Leben auf der „Fernen, fernen Weite“ beginnt.
-
-Jeder Tag bringt neue Herausforderungen und Gefahren, während du versuchst, mit der Haltung von Slimes Reichtümer anzuhäufen. Sammle bunte Slimes, baue Feldfrüchte an, ernte Ressourcen und erforsche die Wildnis, indem du den Umgang mit deinem Vac-Pack meisterst.
-
-Erstellt von Monomi Park
-",
-	/* 06 */ "
-<t>Metro Exodus</t>
-Flüchte aus den Ruinen der Moskauer Metro und begib dich auf eine beschwerliche Reise durch die postapokalyptische Wildnis Russlands. Erkunde weitläufige, nicht-lineare Level und folge diesem packenden Sandbox-Survival-Erlebnis mit seiner fesselnden Geschichte in das größte Metroabenteuer, das es je gab.
-
-Nutzt einen Drittanbieter-Kopierschutz: Denuvo Anti-Tamper
-
-Erstellt von 4A Games \ Veröffentlicht durch Deep Silver
-",
-	/* 07 */ "
-<t>Kämpf dich aus der Hölle</t>
-In diesem Dungeon-Crawler von den Machern von Bastion und Transistor widersetzt du dich den Göttern des Todes und findest einen Weg hinaus aus
-​der Unterwelt.
-
-Spiele im Early Access:​​ Es handelt sich um lebendiges Spiel, das sich noch in der Entwicklung befindet! Du kannst mit regelmäßigen Updates, neuen Features, Events, Charakteren, Orten, Kräften und mehr rechnen. ​Details
-
-Erstellt von Supergiant Games
-",
-	/* 08 */ "
-<t>Ashen</t>
-Ashen ist ein Actionrollenspiel, in dem der Spieler die Rolle eines Wanderers übernimmt, der auf der Suche nach einem neuen Zuhause ist. In dieser Welt hat nichts Bestand, ganz egal wie sehr ihr auch daran hängen mögt. Das Schmieden von Freundschaften und Beziehungen ist ein grundlegendes Element von Ashen. Spieler können andere, denen sie vertrauen, zu ihrem eigenen Lager führen, denn nur gemeinsam werdet ihr eine Überlebenschance haben.
-
-Erstellt von A44 / Annapurna Interactive
-",
-	/* 09 */ "
-<t>Vom Gejagten zum Jäger</t>
-Willkommen in der Steinzeit, einer Ära extremer Gefahren, als gigantische Mammuts und Säbelzahntiger die Erde beherrschten und die Menschheit sich am unteren Ende der Nahrungskette befand. Als letzter Überlebender deiner Gruppe von Jägern lernst du, tödliche Waffen zu fertigen, wilde Raubtiere abzuwehren und gegnerische Stämme zu überlisten, um das Land Oros zu erobern und an die Spitze der Nahrungskette aufzusteigen.
-
-DIE STEINZEIT VON FAR CRY: Die vielfältige Kulisse der Steinzeit bietet eine neue Umgebung mit unvorhersehbarem Chaos, gefährlichen Bedrohungen und unglaublichen Geschichten, für die Far Cry bekannt ist.
-
-WIDERSETZE DICH DEM AUSSTERBEN: Spiele als Takkar, ein erfahrener Jäger und letztes überlebendes Mitglied seiner Gruppe. Es gibt nur ein Ziel: in einer Welt überleben, in der du die Beute bist.
-
-GEFÄHRLICHE WILDNIS UND EINE URTÜMLICHE UND MAJESTÄTISCHE WELT: Dies ist die Erde, bevor der Mensch sie eroberte, in der tödliche Säbelzahntiger die Welt beherrschten, riesige Wollmammuts die Könige waren und Herden von Riesenhirschen über die Ebenen zogen. Entdecke diese und viele weitere furchterregende Geschöpfe in der wilden und weiten Welt von Oros mit ihren mächtigen Mammutbaumwäldern, ihrer rauen Taiga, ihren kalten Gletscherbergen und den schwülen Sümpfen.
-",
-	/* 10 */ "
-<t>Darksiders III</t>
-Kehre in diesem Hack-’n’-Slash-Action-Rollenspiel auf eine apokalyptische Erde zurück und spiele als FURY, die Jagd auf die sieben Todsünden macht, um sie auszulöschen. Als unberechenbarste der vier apokalyptischen Reiter muss FURY nun beenden, wo die anderen scheiterten – die Harmonie unter den Kräften wiederherzustellen, die nun die Erde verwüsten.
-
-Erstellt von Gunfire Games / Published by THQ Nordic
-",
-	/* 11 */ "
-<t>Tom Clancy’s Ghost Recon® Wildlands Standard Edition</t>
-Tom Clancy’s Ghost Recon® Wildlands ist der allererste Militär-Shooter, in dem du allein oder zu viert im Koop-Modus in einer gigantischen, lebendigen Spielwelt kämpfst.
-
-ZERSCHLAGE DAS KARTELL
-In nicht allzu ferner Zukunft wird Bolivien zum größten Kokainproduzenten der Welt. Das Santa-Blanca-Kartell hat das Land in einen Drogenstaat verwandelt. Es ist deine Aufgabe als Ghost, dem Kartell mit allen Mitteln ein Ende zu bereiten.
-
-WERDE EIN GHOST
-Erstelle und passe deinen Ghost vollständig an, einschließlich seiner Waffen und Ausrüstung. Genieße ein völlig freies Spielerlebnis. Führe dein Team an und zerschlage das Kartell, auf eigene Faust oder mit bis zu drei Freunden.
-
-ERKUNDE BOLIVIEN
-Reise durch Ubisoft’s größte offene Welt voller Action und Abenteuer. Entdecke die atemberaubende und vielfältige Landschaft entlang der Straßen oder fern ab davon. Nutze dazu über 60 verschiedene Fahrzeuge zu Land, zu Wasser und in der Luft.
-",
-	/* 12 */ "
-<t>Watch Dogs 2</t>
-In der riesigen und dynamischen Welt von Watch Dogs 2 ist Hacken deine Waffe.
-
-Im Jahr 2016 wurde ctOS 2.0, ein fortschrittliches Betriebssystem zur Vernetzung der städtischen Infrastruktur, in mehreren US-Städten implementiert, um eine sicherere und effizientere Metropole zu schaffen.
-
-Spiele als Marcus Holloway, ein brillanter junger Hacker, der im Geburtsort der technischen Revolution, der San Francisco Bay Area, lebt. Schließe dich DedSec, einer berüchtigten Gruppe von Hackern, an und decke die verborgenen Gefahren von ctOS 2.0 auf, das in den Händen korrupter Unternehmen missbraucht wird, um Bürger in großem Umfang zu überwachen und zu manipulieren.
-
-Starte mit deinen Hacking-Fähigkeiten und DedSec den Hack des Jahrhunderts, nimm ctOS 2.0 vom Netz und gib denen die Freiheit wieder, denen sie gebührt: den Menschen.
-
-WILLKOMMEN IN DER BAY AREA VON SAN FRANCISCO: Entdecke eine massive, dynamische und offene Welt, die eine unglaubliche Vielfalt an Spielmöglichkeiten bietet: Hacke dir deinen Weg durch den Verkehr der kurvenreichen Straßen von San Francisco oder infiltriere die hochmodernen Büros des Silicon Valley.
-
-HACKEN IST DEINE WAFFE: Hacke dich in die Infrastruktur der Stadt, jedes verbundene Gerät und jede persönliche Datenbank. Übernimm die Kontrolle über Drohnen, Autos, Kräne, Sicherheitsroboter und vieles mehr.
-
-DU ÜBERNIMMST DIE STRG: Schließe mithilfe von Hacking und Täuschung Missionen ab oder kombiniere das Hacken mit deinen Waffen. Rüste deine Hacker-Tools auf – ferngesteuerte Autos, Quadcopter-Drohnen, Waffen aus dem 3D-Drucker und vieles mehr.
-
-NAHTLOS MIT FREUNDEN VERBINDEN: Bleibe mit deinen Freunden in einer brandneuen Mehrspieler-Erfahrung in Verbindung, die sowohl Koop- als auch Spieler-gegen-Spieler-Aktivitäten enthält.
-
-Erstellt von Ubisoft Entertainment
-",
-	/* 13 */ "
-<t>Outlive the dead</t>
-World War Z ist ein adrenalinreicher Koop-Shooter für vier Spieler, in dem gigantische Schwärme von Untoten ihre lebende Beute gnadenlos verfolgen.
-
-Mit einem Auge auf rasantem Gameplay und inspiriert vom unglaublich beliebten Paramount Pictures-Film erforscht World War Z neue Geschichten und Charaktere aus der gesamten Welt. Erlebt aufregende, überwältigende und grauenhaft spannende Missionen, eigens entwickelt für moderne PCs.
-
-Kämpfen Sie in unglaublichen Gefechten gegen hunderte Zombies
-Erleben Sie geschichtslastige Koop-Kampagnen
-Kämpfen Sie in Spieler vs. Spieler vs. Zombies-Modi
-Leveln Sie 6 Charakter-Klassen und schalten Sie mächtige Verbesserungen frei
-World War Z vorbestellen und die berüchtigte Lobo-Nahkampfwaffe sowie drei goldene Waffenskins erhalten, um die Zombies stilvoll zu erledigen!
-
-Erstellt von Saber Interactive
-"
-);
+        array_push($product_name, $row_products['product_name']);
+        array_push($product_developer, $row_products['product_developer']);
+        array_push($product_description, $row_products['product_description']);
+        array_push($product_editions, array($row_editions['edition_1'], $row_editions['edition_2'], $row_editions['edition_3'], $row_editions['edition_4']));
+        array_push($product_price, array($row_prices['price_1'], $row_prices['price_2'], $row_prices['price_3'], $row_prices['price_4']));
+    }
 ?>
